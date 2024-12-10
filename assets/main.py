65ce4d08f3185c36b631891f356e76630e1ae585
Fernando Vargas <nandovargas7@gmail.com>
2024-12-10 11:45:12 -0300

    Servicos e integração com o db mockado

--- assets/main.py
+++ assets/main.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
-
+from database import Base, engine
 
 app = FastAPI()
 
+Base.metadata.create_all(bind=engine)
+
 @app.get("/")
 def read_root():
-    return {"message": "Bem vindo a API de Gestão de ativos"}
+    return {"message": "FastAPI com Docker e PostgreSQL!"}
