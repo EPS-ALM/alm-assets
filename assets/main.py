32023ddb77a66497350773b9a7c3125f0e629924
Lucas Pimentel <luk.2001@hotmail.com>
2024-12-12 17:51:42 -0300

    fix: removes db and examples

--- assets/main.py
+++ assets/main.py
@@ -1,14 +1,9 @@
 from fastapi import FastAPI
-from database import Base, engine
-from views.item_view import router as item_router
 from views.markowitz import markowitz
 from views.forecast_var_view import forecast_var
 
 app = FastAPI()
 
-Base.metadata.create_all(bind=engine)
-
-app.include_router(item_router)
 app.include_router(markowitz)
 app.include_router(forecast_var)
 
