4f4ac8d5aae5d47ad6f569b4a679400ec8cb49da
ArthurFerreiraRodrigues <arthur.250402@gmail.com>
2024-12-15 16:52:21 -0300

    Adiciona controller para o forecast usando lstm

--- assets/controllers/forecast_lstm_controller.py
+++ assets/controllers/forecast_lstm_controller.py
@@ -1,6 +1,10 @@
-from ...stocks_forecasting.modelos.LSTM import forecast_with_lstm
+from ...stocks_forecasting.modelos.LSTM import forecast_with_lstm, get_avaible_models
 
-def forecast(ticker:str):
+def forecast_lstm(ticker:str):
     """Realiza previsão de retornos de um ativo usando LSTM."""
-    forecast = forecast_with_lstm(ticker)
+    avaible_models = get_avaible_models()
+    if ticker not in avaible_models:
+        raise ValueError(f"Modelo para {ticker} não encontrado.")
+    
+    return forecast_with_lstm(ticker)
     
