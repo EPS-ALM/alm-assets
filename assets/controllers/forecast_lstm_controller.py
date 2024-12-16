d20fc60eda8f6e2df9eb1fc864bafe111f719e98
augustocrmg <augusto.duraes33@gmail.com>
2024-12-16 01:45:13 -0300

    Modificação controller que chama a LSTM
    Co-authored-by: ArthurFerreiraRodrigues <arthur.250402@gmail.com>

--- assets/controllers/forecast_lstm_controller.py
+++ assets/controllers/forecast_lstm_controller.py
@@ -1,4 +1,6 @@
-from ...stocks_forecasting.modelos.LSTM import forecast_with_lstm, get_avaible_models
+import yfinance as yf
+import datetime as dt
+from ...stocks_forecasting.modelos.LSTM import forecast_with_lstm, get_avaible_models, get_model_prediction
 
 def forecast_lstm(ticker:str):
     """Realiza previsão de retornos de um ativo usando LSTM."""
@@ -6,5 +8,15 @@ def forecast_lstm(ticker:str):
     if ticker not in avaible_models:
         raise ValueError(f"Modelo para {ticker} não encontrado.")
     
-    return forecast_with_lstm(ticker)
+    final = dt.datetime.now().date()
+    inicial = dt.date(final.year - 20, 1, 1)
+    
+    df = yf.download(ticker, inicial, final)
+    df.columns = [header_pos[0] for header_pos in df.columns]
+    df = df.reset_index()
+    #df.to_csv(f'../../stocks_forecasting/dados/raw/{ticker}.csv')
+    
+    lstm_forecast = get_model_prediction(ticker, df)
+    
+    return lstm_forecast
     
